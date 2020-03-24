3d6490880b27fdc0ecc6b91c42a70f112d5d27a6
root <lijia0732@sina.com>
2020-03-24 18:53:23 +0800

    V4.6 Update

--- src/nicelee/ui/Global.java
+++ src/nicelee/ui/Global.java
@@ -9,7 +9,7 @@ import nicelee.ui.item.DownloadInfoPanel;
 
 public class Global {
 	// 界面显示相关
-	public static String version = "v4.5";
+	public static String version = "v4.6";
 	public static boolean themeDefault = true;
 	public static FrameWaiting frWaiting;
 	public static boolean lockCheck = false;

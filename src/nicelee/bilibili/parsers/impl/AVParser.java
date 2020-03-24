490531faa4396bd2f67d6e91786d19628c8372d2
root <lijia0732@sina.com>
2020-03-24 18:32:39 +0800

    修复Av解析

--- src/nicelee/bilibili/parsers/impl/AVParser.java
+++ src/nicelee/bilibili/parsers/impl/AVParser.java
@@ -2,9 +2,11 @@ package nicelee.bilibili.parsers.impl;
 
 import java.util.regex.Pattern;
 
+import org.json.JSONObject;
 
 import nicelee.bilibili.annotations.Bilibili;
 import nicelee.bilibili.model.VideoInfo;
+import nicelee.bilibili.util.HttpHeaders;
 
 @Bilibili(name = "av")
 public class AVParser extends AbstractBaseParser {
@@ -36,7 +38,29 @@ public class AVParser extends AbstractBaseParser {
 	@Override
 	public VideoInfo result(String input, int videoFormat, boolean getVideoLink) {
 		System.out.println("AVParser正在获取结果" + avId);
-		return getAVDetail(avId, videoFormat, getVideoLink);
+		return getAVDetail(AvIdToBvId(avId), videoFormat, getVideoLink);
 	}
 
+	/**
+	 * 已知avId, 求BvId 目前没有抓到api哦... 暂时从网页里面爬
+	 * 
+	 * @input HttpRequestUtil util
+	 */
+	private String AvIdToBvId(String avId) {
+		HttpHeaders headers = new HttpHeaders();
+		String url = "https://www.bilibili.com/video/" + avId;
+		String html = util.getContent(url, headers.getCommonHeaders("www.bilibili.com"));
+
+		int begin = html.indexOf("window.__INITIAL_STATE__=");
+		int end = html.indexOf(";(function()", begin);
+		String json = html.substring(begin + 25, end);
+		System.out.println(json);
+		JSONObject jObj = new JSONObject(json);
+		String bvId = jObj.getString("bvid");
+		if(bvId.isEmpty()) {
+			bvId = jObj.getJSONObject("videoData").getString("bvid");
+		}
+		System.out.println("bvId为: " + bvId);
+		return bvId;
+	}
 }

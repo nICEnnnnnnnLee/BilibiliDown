bf8365aecd21748068dfffa34ec5cf3fbf956482
root <lijia0732@sina.com>
2020-04-11 14:21:23 +0800

    增加了不依赖IDE的打包脚本(内含删除操作,请勿随意移动脚本位置)

---
+++ package.bat
@@ -0,0 +1,40 @@
+:: 复制整个文件夹
+xcopy src target\ /s /f /h
+
+:: 删除不需要的java文件
+rmdir /s/q target\nicelee\test\
+
+:: 获取java文件列表
+cd target
+dir /s /B *.java > ../sources.txt
+cd ..
+
+:: 获取环境变量,解压lib包
+cd libs
+dir /s /B *.jar > ../libs.txt
+cd ../target
+setlocal enabledelayedexpansion
+set classpath=.
+for /f "tokens=*" %%i in (../libs.txt) do (
+set classpath=!classpath!;%%i
+jar xvf %%i
+)
+cd ..
+
+:: 编译java
+javac -cp !classpath! -encoding UTF-8 @sources.txt
+
+:: 删除所有.java文件
+cd target
+del /a /f /s /q  "*.java"
+cd ..
+
+:: 打包
+jar cvfe INeedBiliAV.jar nicelee.ui.FrameMain -C ./target .
+
+echo 按任意键删除临时文件
+pause
+
+rmdir /s/q  target\
+del sources.txt
+del libs.txt
---
+++ package.sh
@@ -0,0 +1,38 @@
+# 复制整个文件夹
+mkdir target
+cp -r src/. target
+
+# 删除不需要的java文件
+rm -rf ./target/nicelee/test
+
+# 获取java文件列表
+cd target
+find `pwd` -name "*.java" > ../sources.txt
+cd ..
+
+# 获取环境变量,解压lib包
+cd libs
+find `pwd` -name "*.jar" > ../libs.txt
+cat ../libs.txt
+cd ../target
+for jar in  `cat ../libs.txt`
+do
+    jclasspath=$jar:$jclasspath
+    jar xvf $jar
+done
+cd ..
+
+# 编译java
+javac -cp $jclasspath -encoding UTF-8 @sources.txt
+
+# 删除所有.java文件
+cd target
+find . -name "*.java" |xargs rm -rf {}
+cd ..
+
+# 打包
+jar cvfe INeedBiliAV.jar nicelee.ui.FrameMain -C ./target .
+
+rm -rf target
+rm -f sources.txt
+rm -f libs.txt

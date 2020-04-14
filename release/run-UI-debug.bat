4b3653112f246a6eaaef233c7e994a5655b1997a
root <lijia0732@sina.com>
2020-04-14 14:20:35 +0800

    增加JRE环境变量设置操作(可选)

--- release/run-UI-debug.bat
+++ release/run-UI-debug.bat
@@ -1,2 +1,5 @@
+rem 如果没有java环境,且下载的是精简jre
+:: set Path=D:\Program Files\Java\minimal-bilibilidown-jre\bin\;%Path%
 cd /d %~dp0
 java -Dfile.encoding=utf-8 -jar INeedBiliAV.jar
+pause
--- release/run-UI.bat
+++ release/run-UI.bat
@@ -1,2 +1,4 @@
+rem 如果没有java环境,且下载的是精简jre
+:: set Path=D:\Program Files\Java\minimal-bilibilidown-jre\bin\;%Path%
 cd /d %~dp0
 start javaw -Dfile.encoding=utf-8 -jar INeedBiliAV.jar

2e2c4220525adc102feb99ddc0c57a3e226afd9f
lijia <lijia0732@sina.com>
2021-03-04 08:40:09 +0800

    标签页增加了右键菜单，可实现批量下载功能(优先清晰度跟随Tab默认值`bilibili.tab.download.qn`)

--- src/nicelee/ui/item/MJTabVideo.java
+++ src/nicelee/ui/item/MJTabVideo.java
@@ -1,26 +1,31 @@
 package nicelee.ui.item;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JTabbedPane;
 
+import nicelee.bilibili.enums.VideoQualityEnum;
+import nicelee.ui.Global;
 import nicelee.ui.TabVideo;
 
+// 关闭当前Tab
 // 关闭所有Tab
 // 关闭左侧Tab
 // 关闭右侧Tab
-public class MJTabVideo extends TabVideo implements MouseListener, ActionListener {
+//---------------
+// 下载当前Tab
+// 下载全部Tab
+public class MJTabVideo extends TabVideo {// implements MouseListener, ActionListener
 
 	private static final long serialVersionUID = 10112L;
 
 	private JPopupMenu pop = null; // 弹出菜单
 	private JMenuItem closeAll = null, closeRight = null, closeLeft = null, closeThis = null; // 功能菜单
+	private JMenuItem downloadAll = null, downloadThis = null;// 功能菜单
 	private JLabel label = null;
 	private JTabbedPane jTabbedpane = null;
 
@@ -37,17 +42,22 @@ public class MJTabVideo extends TabVideo implements MouseListener, ActionListene
 		pop.add(closeAll = new JMenuItem("关闭所有标签"));
 		pop.add(closeLeft = new JMenuItem("关闭左侧标签"));
 		pop.add(closeRight = new JMenuItem("关闭右侧标签"));
+		pop.addSeparator();
+		pop.add(downloadThis = new JMenuItem("批量下载此标签"));
+		pop.add(downloadAll = new JMenuItem("批量下载全部标签"));
 		closeThis.addActionListener(this);
 		closeAll.addActionListener(this);
 		closeRight.addActionListener(this);
 		closeLeft.addActionListener(this);
+		downloadThis.addActionListener(this);
+		downloadAll.addActionListener(this);
 		label.add(pop);
 		label.addMouseListener(this);
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if(e.getSource() == label) {
+		if (e.getSource() == label) {
 			System.out.println("MJTab label的点击事件");
 			if (e.getButton() == MouseEvent.BUTTON1) {
 				// 鼠标左键双击事件， 删除此tab页
@@ -58,7 +68,7 @@ public class MJTabVideo extends TabVideo implements MouseListener, ActionListene
 					jTabbedpane.setSelectedComponent(this);
 				}
 			}
-		}else {
+		} else {
 			super.mouseClicked(e);
 		}
 	}
@@ -66,9 +76,9 @@ public class MJTabVideo extends TabVideo implements MouseListener, ActionListene
 	@Override
 	public void mousePressed(MouseEvent e) {
 		if (e.getSource() == label) {
-			if(e.getButton() == MouseEvent.BUTTON3)
-			pop.show(label, e.getX(), e.getY());
-		}else {
+			if (e.getButton() == MouseEvent.BUTTON3)
+				pop.show(label, e.getX(), e.getY());
+		} else {
 			super.mousePressed(e);
 		}
 	}
@@ -112,7 +122,13 @@ public class MJTabVideo extends TabVideo implements MouseListener, ActionListene
 			for (int i = jTabbedpane.getTabCount() - 1; i > currentTabIndex; i--) {
 				jTabbedpane.removeTabAt(i);
 			}
-		}else {
+		} else if (e.getSource() == downloadThis) {
+			int qn = VideoQualityEnum.getQN(Global.tab_qn);
+			this.download(true, qn);
+		} else if (e.getSource() == downloadAll) {
+			int qn = VideoQualityEnum.getQN(Global.tab_qn);
+			Global.index.downVideoTabs(true, qn);
+		} else {
 			super.actionPerformed(e);
 		}
 	}

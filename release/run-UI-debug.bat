6761a64ddceb3c9c1323fd1e942f0a71aa4a4708
lijia <lijia0732@sina.com>
2022-07-14 21:55:03 +0800

    新增：Release打包增加了附带精简jre11的选项, 为没有Java环境的win64用户提供了另一种可能。

--- release/run-UI-debug.bat
+++ release/run-UI-debug.bat
@@ -1,5 +1,6 @@
-rem 如果没有java环境,且下载的是精简jre
-:: set Path=D:\Program Files\Java\minimal-bilibilidown-jre\bin\;%Path%
 cd /d %~dp0
+:: 将精简jre写入当前环境的path
+:: set Path=D:\Program Files\Java\minimal-bilibilidown-jre\bin\;%Path%
+set Path=%~dp0minimal-bilibilidown-jre;%Path%
 java -Dfile.encoding=utf-8 -jar INeedBiliAV.jar
 pause

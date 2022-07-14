21cdd6020da15d62fc413a2db0318e6e563a4fc1
lijia <lijia0732@sina.com>
2022-07-14 22:43:18 +0800

    纠正错误的path设置

--- release/run-UI-debug.bat
+++ release/run-UI-debug.bat
@@ -1,6 +1,6 @@
 cd /d %~dp0
 :: 将精简jre写入当前环境的path
-:: set Path=D:\Program Files\Java\minimal-bilibilidown-jre\bin\;%Path%
-set Path=%~dp0minimal-bilibilidown-jre;%Path%
+:: set Path=D:\Program Files\Java\minimal-bilibilidown-jre\bin;%Path%
+set Path=%~dp0minimal-bilibilidown-jre\bin\;%Path%
 java -Dfile.encoding=utf-8 -jar INeedBiliAV.jar
 pause
--- release/run-UI.bat
+++ release/run-UI.bat
@@ -1,4 +1,4 @@
 cd /d %~dp0
 :: 将精简jre写入当前环境的path
-set Path=%~dp0minimal-bilibilidown-jre;%Path%
+set Path=%~dp0minimal-bilibilidown-jre\bin;%Path%
 start javaw -Dfile.encoding=utf-8 -jar INeedBiliAV.jar

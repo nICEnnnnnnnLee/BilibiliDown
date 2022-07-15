6e72c9d7ddda4e3b8abea03d0ba67951a25c2945
lijia <lijia0732@sina.com>
2022-07-15 22:36:38 +0800

    运行时增加`-Dhttps.protocols=TLSv1.2`参数，防止tls handshake failure

--- release/run-UI.bat
+++ release/run-UI.bat
@@ -1,4 +1,5 @@
 cd /d %~dp0
 :: 将精简jre写入当前环境的path
 set Path=%~dp0minimal-bilibilidown-jre\bin;%Path%
-start javaw -Dfile.encoding=utf-8 -jar INeedBiliAV.jar
+:: -Dhttps.protocols=TLSv1.2 https://bugs.openjdk.org/browse/JDK-8206923
+start javaw -Dfile.encoding=utf-8 -Dhttps.protocols=TLSv1.2 -jar INeedBiliAV.jar

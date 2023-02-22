c46d10e8b05a7da0388c7bb26be427576d3810e1
lijia <lijia0732@sina.com>
2023-02-22 12:56:13 +0800

    feat: 运行脚本将目标jar包由`INeedBiliAV.jar`改为`launch.jar`

--- release/Double-Click-to-Run-for-Win.bat
+++ release/Double-Click-to-Run-for-Win.bat
@@ -2,4 +2,5 @@ cd /d %~dp0
 :: 将精简jre写入当前环境的path
 set Path=%~dp0minimal-bilibilidown-jre\bin;%Path%
 :: -Dhttps.protocols=TLSv1.2 https://bugs.openjdk.org/browse/JDK-8206923
-start javaw -Dfile.encoding=utf-8 -Dhttps.protocols=TLSv1.2 -jar INeedBiliAV.jar
+:: start javaw -Dhttps.protocols=TLSv1.2 -jar INeedBiliAV.jar
+start javaw -Dhttps.protocols=TLSv1.2 -jar launch.jar

835280496c6798e01a9603133f87b89db294acb3
lijia <lijia0732@sina.com>
2023-03-19 15:51:21 +0800

    refactor: 重写`findClass`而不是`loadClass`

--- src-launcher/nicelee/memory/classloader/MemoryClassLoader.java
+++ src-launcher/nicelee/memory/classloader/MemoryClassLoader.java
@@ -10,16 +10,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 
 import nicelee.memory.url.MemoryURLHandler;
 
 public class MemoryClassLoader extends ClassLoader {
 
 	private static boolean DEBUG = false;
-	private static boolean DEBUGLOADCLASS = false;
-	@SuppressWarnings("rawtypes")
-	private static ConcurrentHashMap<String, Class> cache = new ConcurrentHashMap<String, Class>();
+//	private static boolean DEBUGLOADCLASS = false;
+//	@SuppressWarnings("rawtypes")
+//	private static ConcurrentHashMap<String, Class> cache = new ConcurrentHashMap<String, Class>();
 
 	final private ProtectionDomain defaultPd;
 
@@ -37,7 +36,7 @@ public class MemoryClassLoader extends ClassLoader {
 			System.out.printf("findResource: %s%n", sName);
 		try {
 			URL u = this.getParent().getResource(sName);
-			if(u == null) {
+			if (u == null) {
 				if (DEBUG)
 					System.out.printf("try findResource from mem: %s%n", sName);
 				String url = "mem://file/" + sName;
@@ -69,73 +68,83 @@ public class MemoryClassLoader extends ClassLoader {
 		throw new RuntimeException("Not Implemented");
 	}
 
-	@SuppressWarnings("rawtypes")
+	// 没有必要重写 loadClass
 	@Override
-	protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-		if (DEBUG)
-			System.out.println("DEBUG:" + this + ".loadClass(" + name + ")");
-		Class c = null;
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
 		try {
-			c = Class.forName(name); // can it be loaded by normal means? and initialized?
-			return c;
-		} catch (Exception e) {
-			if (DEBUGLOADCLASS) {
-				System.out.println("DEBUG:" + this + ".loadClass Class.forName(" + name + ") exception " + e);
-				// e.printStackTrace();
-			}
+			return findMemoryClass(name);
+		} catch (IOException e) {
+			throw new ClassNotFoundException("No such class in memory", e);
 		}
-		try {
-			c = findSystemClass(name);
-		} catch (Exception e) {
-			if (DEBUGLOADCLASS) {
-				System.out.println("DEBUG:" + this + ".loadClass findSystemClass(" + name + ") exception " + e);
-				// e.printStackTrace();
-			}
-		}
-		if (c == null) {
-			c = cache.get(name);
-		} else {
-			if (DEBUG)
-				System.out.println("DEBUG:" + this + ".loadClass exit found sys class " + name + " resolve=" + resolve);
-			return c;
-		}
-		if (c == null) {
-			c = findLoadedClass(name);
-		} else {
-			if (DEBUG)
-				System.out.println(
-						"DBUG:" + this + ".loadClass exit cache hit:" + c + " for " + name + " resolve=" + resolve);
-			return c;
-		}
-		// this is our last chance, otherwise noClassDefFoundErr and we're screwed
-		if (c == null) {
-			try {
-				c = findMemoryClass(name);
-				if (DEBUG)
-					System.out.println("DEBUG:" + this + " Putting class " + name + " of class " + c + " to cache");
-				cache.put(name, c);
-			} catch (IOException e) {
-				e.printStackTrace();
-				if (e.getCause() == null) {
-					System.out.printf("Not found %s in JAR by %s: %s%n", name, getClass().getName(), e.getMessage());
-				} else {
-					System.out.printf("Error loading %s in JAR by %s: %s%n", name, getClass().getName(), e.getCause());
-				}
-				// end of the line
-				throw new ClassNotFoundException(
-						"The requested class: " + name + " can not be found on any resource path");
-			}
-		} else {
-			if (DEBUG)
-				System.out.println("DEBUG:" + this + ".loadClass exit found loaded " + name + " resolve=" + resolve);
-			return c;
-		}
-		if (resolve)
-			resolveClass(c);
-		if (DEBUG)
-			System.out.println("DEBUG:" + this + ".loadClass exit resolved " + name + " resolve=" + resolve);
-		return c;
 	}
+
+//	@SuppressWarnings("rawtypes")
+//	@Override
+//	protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+//		if (DEBUG)
+//			System.out.println("DEBUG:" + this + ".loadClass(" + name + ")");
+//		Class c = null;
+//		try {
+//			c = Class.forName(name); // can it be loaded by normal means? and initialized?
+//			return c;
+//		} catch (Exception e) {
+//			if (DEBUGLOADCLASS) {
+//				System.out.println("DEBUG:" + this + ".loadClass Class.forName(" + name + ") exception " + e);
+//				// e.printStackTrace();
+//			}
+//		}
+//		try {
+//			c = findSystemClass(name);
+//		} catch (Exception e) {
+//			if (DEBUGLOADCLASS) {
+//				System.out.println("DEBUG:" + this + ".loadClass findSystemClass(" + name + ") exception " + e);
+//				// e.printStackTrace();
+//			}
+//		}
+//		if (c == null) {
+//			c = cache.get(name);
+//		} else {
+//			if (DEBUG)
+//				System.out.println("DEBUG:" + this + ".loadClass exit found sys class " + name + " resolve=" + resolve);
+//			return c;
+//		}
+//		if (c == null) {
+//			c = findLoadedClass(name);
+//		} else {
+//			if (DEBUG)
+//				System.out.println(
+//						"DBUG:" + this + ".loadClass exit cache hit:" + c + " for " + name + " resolve=" + resolve);
+//			return c;
+//		}
+//		// this is our last chance, otherwise noClassDefFoundErr and we're screwed
+//		if (c == null) {
+//			try {
+//				c = findMemoryClass(name);
+//				if (DEBUG)
+//					System.out.println("DEBUG:" + this + " Putting class " + name + " of class " + c + " to cache");
+//				cache.put(name, c);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//				if (e.getCause() == null) {
+//					System.out.printf("Not found %s in JAR by %s: %s%n", name, getClass().getName(), e.getMessage());
+//				} else {
+//					System.out.printf("Error loading %s in JAR by %s: %s%n", name, getClass().getName(), e.getCause());
+//				}
+//				// end of the line
+//				throw new ClassNotFoundException(
+//						"The requested class: " + name + " can not be found on any resource path");
+//			}
+//		} else {
+//			if (DEBUG)
+//				System.out.println("DEBUG:" + this + ".loadClass exit found loaded " + name + " resolve=" + resolve);
+//			return c;
+//		}
+//		if (resolve)
+//			resolveClass(c);
+//		if (DEBUG)
+//			System.out.println("DEBUG:" + this + ".loadClass exit resolved " + name + " resolve=" + resolve);
+//		return c;
+//	}
 
 	private Class<?> findMemoryClass(String sClassName) throws IOException {
 		String url = "mem://file/" + sClassName.replace('.', '/') + ".class";
--- src-launcher/nicelee/memory/url/MemoryURLHandler.java
+++ src-launcher/nicelee/memory/url/MemoryURLHandler.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLStreamHandler;

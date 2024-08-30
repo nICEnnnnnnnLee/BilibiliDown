faa370a0cfbe03f2e53b8723e5090a34f17bc071
lijia <lijia0732@sina.com>
2024-08-30 21:39:44 +0800

    ui: 备用入口类同步基础功能

--- src/nicelee/ui/FrameMain_v3_4.java
+++ src/nicelee/ui/FrameMain_v3_4.java
@@ -11,12 +11,22 @@ import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
 
+import nicelee.bilibili.INeedAV;
 import nicelee.bilibili.INeedLogin;
+import nicelee.bilibili.PackageScanLoader;
+import nicelee.bilibili.model.VideoInfo;
 import nicelee.bilibili.util.CmdUtil;
 import nicelee.bilibili.util.ConfigUtil;
+import nicelee.bilibili.util.HttpCookies;
 import nicelee.bilibili.util.Logger;
 import nicelee.bilibili.util.RepoUtil;
+import nicelee.bilibili.util.ResourcesUtil;
+import nicelee.bilibili.util.SysUtil;
+import nicelee.ui.item.JOptionPane;
 import nicelee.ui.item.MJMenuBar;
+import nicelee.ui.thread.BatchDownloadRbyRThread;
+import nicelee.ui.thread.CookieRefreshThread;
+import nicelee.ui.thread.DownloadRunnable;
 import nicelee.ui.thread.LoginThread;
 import nicelee.ui.thread.MonitoringThread;
 
@@ -29,35 +39,123 @@ public class FrameMain_v3_4 extends JFrame {
 	JTabbedPane jTabbedpane;// 存放选项卡的组件
 
 	public static void main(String[] args) {
+		System.out.println();
 		// System.getProperties().setProperty("file.encoding", "utf-8");
+		boolean isFFmpegSurported = SysUtil.surportFFmpegOfficially();
+		System.out.println("Java version:" + System.getProperty("java.specification.version"));
+		System.out.println(ResourcesUtil.baseDirectory());
+		// 读取配置文件
 		ConfigUtil.initConfigs();
-		//初始化主题
-		initUITheme();
+		// -v 打印版本，然后退出
+		if(args.length == 1 && "-v".equalsIgnoreCase(args[0])) {
+			System.out.println(Global.version);
+			System.exit(0);
+		}
+		// 显示过渡动画
+		Global.frWaiting = new FrameWaiting();
+		Global.frWaiting.start();
+
+		if (Global.lockCheck) {
+			if (ConfigUtil.isRunning()) {
+				Global.frWaiting.stop();
+				JOptionPane.showMessageDialog(null, "程序已经在运行!", "请注意!!", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			ConfigUtil.createLock();
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				ConfigUtil.deleteLock();
+			}));
+		}
 		
-		//初始化UI
+		nicelee.bilibili.util.custom.System.init(Global.syncServerTime);
+		// 初始化主题
+		initUITheme();
+
+		// 初始化UI
 		FrameMain_v3_4 main = new FrameMain_v3_4();
 		main.InitUI();
-		
-		//初始化监控线程，用于刷新下载面板
+
+		// 初始化监控线程，用于刷新下载面板
 		MonitoringThread th = new MonitoringThread();
 		th.start();
 
-		//初始化 - 登录
+		// 尝试刷新cookie
 		INeedLogin inl = new INeedLogin();
-		if (inl.readCookies() != null) {
+		String cookiesStr = inl.readCookies();
+		if (cookiesStr != null) {
 			Global.needToLogin = true;
+			if(Global.tryRefreshCookieOnStartup && !Global.runWASMinBrowser) {
+				HttpCookies.setGlobalCookies(HttpCookies.convertCookies(cookiesStr));
+				CookieRefreshThread.showTips = false;
+				CookieRefreshThread thCR = CookieRefreshThread.newInstance();
+				thCR.start();
+				try {
+					thCR.join();
+				} catch (InterruptedException e1) {
+				}
+				CookieRefreshThread.showTips = true;
+			}
 		}
+		// 初始化 - 登录
 		LoginThread loginTh = new LoginThread();
 		loginTh.start();
 
+		// 初始化 - ffmpeg环境判断
+		CmdUtil.DEFAULT_WORKING_DIR = ResourcesUtil.baseDirFile();
+		String ffmpeg = ResourcesUtil.resolve(Global.ffmpegPath);
+		String[] cmd = new String[] { ffmpeg, "-version" };
+		if (!CmdUtil.run(cmd)) {
+			if (isFFmpegSurported) {
+				Object[] options = { "是", "否" };
+				int m = JOptionPane.showOptionDialog(null,
+						"检测到当前没有ffmpeg环境, mp4及小部分flv文件将无法转码或合并.\r\n     是否下载ffmpeg(自编译, 3M左右)?", "请选择：",
+						JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+				Logger.println(m);
+				if (m == 0) {
+					VideoInfo avInfo = new INeedAV().getVideoDetail("ffmpeg", 0, false);
+					DownloadRunnable downThread = new DownloadRunnable(avInfo, avInfo.getClips().get(1234L), 0);
+					Global.queryThreadPool.execute(downThread);
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "当前没有ffmpeg环境，大部分mp4及小部分flv文件将无法转码或合并", "请注意!!",
+						JOptionPane.WARNING_MESSAGE);
+			}
+		} else {
+			CmdUtil.FFMPEG_PATH = ffmpeg;
+		}
+
 		//
-		if(Global.saveToRepo) {
+		if (Global.saveToRepo) {
 			RepoUtil.init(false);
 		}
-		//Logger.println(.);
-//		FrameQRCode qr = new FrameQRCode("https://www.bilibili.com/");
-//		qr.initUI();
-//		qr.dispose();
+                      
+		// 预扫描加载类
+		PackageScanLoader.validParserClasses.isEmpty();
+		if(Global.batchDownloadRbyRRunOnStartup) {
+			// 开始按计划周期性批量下载
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					// 等待相关线程运行完毕
+					try {
+						loginTh.join();
+					} catch (InterruptedException e) {}
+					new BatchDownloadRbyRThread(Global.batchDownloadConfigName).start();
+				}
+			}).start();
+		}
+		System.out.println("如果过度界面显示时间过长，可双击跳过");
+		try {
+			while (Global.frWaiting.isVisible()) {
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			Global.frWaiting.stop();
+		}
+		Global.frWaiting = null;
+		main.setVisible(true);
+		main.setExtendedState(JFrame.NORMAL);
+		main.toFront();
 	}
 
 	/**
@@ -123,9 +221,7 @@ public class FrameMain_v3_4 extends JFrame {
 				CmdUtil.deleteAllInactiveCmdTemp();
 			}
 		});
-		this.setVisible(true);
-		Logger.println(jTabbedpane.getWidth());
-		Logger.println(jTabbedpane.getHeight());
+		SysTray.buildSysTray(this, icon.getImage());
 	}
 
 }

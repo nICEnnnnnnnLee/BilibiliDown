6bfcbec3f0ef754924e9dff4bbed3c95806649a5
lijia <lijia0732@sina.com>
2024-09-02 13:58:40 +0800

    refactor: 重构并优化ffmpeg检查

--- src/nicelee/ui/FrameMain_v3_4.java
+++ src/nicelee/ui/FrameMain_v3_4.java
@@ -41,7 +41,7 @@ public class FrameMain_v3_4 extends JFrame {
 	public static void main(String[] args) {
 		System.out.println();
 		// System.getProperties().setProperty("file.encoding", "utf-8");
-		boolean isFFmpegSurported = SysUtil.surportFFmpegOfficially();
+		boolean isFFmpegSupported = SysUtil.surportFFmpegOfficially();
 		System.out.println("Java version:" + System.getProperty("java.specification.version"));
 		System.out.println(ResourcesUtil.baseDirectory());
 		// 读取配置文件
@@ -101,29 +101,7 @@ public class FrameMain_v3_4 extends JFrame {
 		loginTh.start();
 
 		// 初始化 - ffmpeg环境判断
-		CmdUtil.DEFAULT_WORKING_DIR = ResourcesUtil.baseDirFile();
-		String ffmpeg = ResourcesUtil.resolve(Global.ffmpegPath);
-		String[] cmd = new String[] { ffmpeg, "-version" };
-		if (!CmdUtil.run(cmd)) {
-			if (isFFmpegSurported) {
-				Object[] options = { "是", "否" };
-				int m = JOptionPane.showOptionDialog(null,
-						"检测到当前没有ffmpeg环境, mp4及小部分flv文件将无法转码或合并.\r\n     是否下载ffmpeg(自编译, 3M左右)?", "请选择：",
-						JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
-				Logger.println(m);
-				if (m == 0) {
-					VideoInfo avInfo = new INeedAV().getVideoDetail("ffmpeg", 0, false);
-					DownloadRunnable downThread = new DownloadRunnable(avInfo, avInfo.getClips().get(1234L), 0);
-					Global.queryThreadPool.execute(downThread);
-				}
-			} else {
-				JOptionPane.showMessageDialog(null, "当前没有ffmpeg环境，大部分mp4及小部分flv文件将无法转码或合并", "请注意!!",
-						JOptionPane.WARNING_MESSAGE);
-			}
-		} else {
-			CmdUtil.FFMPEG_PATH = ffmpeg;
-		}
-
+		InitCheck.checkFFmpeg(isFFmpegSupported);
 		//
 		if (Global.saveToRepo) {
 			RepoUtil.init(false);
